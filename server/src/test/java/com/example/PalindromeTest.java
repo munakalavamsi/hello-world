dc2ecdcd53f0e9d81a005412f516dd5fb5cb54c0
beeru405 <101712802+beeru405@users.noreply.github.com>
2023-03-02 10:11:43 +0530

    Update and rename TestGreeter.java to PalindromeTest.java

---
+++ server/src/test/java/com/example/PalindromeTest.java
@@ -0,0 +1,23 @@
+
+package com.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class PalindromeTest{
+
+
+@Test
+// Driver code
+public void checkstr()
+{
+    Palindrome palindromeTester = new Palindrome();
+    assertTrue(palindromeTester.isWordPresent(" hello this is abhishek","hello"));
+	
+	
+
+}
+}
