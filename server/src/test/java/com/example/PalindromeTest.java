e1cf6e4dd00186d630c5bc98007d257549c748ec
Munakala Vamsi <munakalavamsi@gmail.com>
2023-04-10 12:30:27 +0530

    hii to hello

--- server/src/test/java/com/example/PalindromeTest.java
+++ server/src/test/java/com/example/PalindromeTest.java
@@ -16,7 +16,7 @@ public class PalindromeTest{
 public void checkstr()
 {
     Palindrome palindromeTester = new Palindrome();
-    assertTrue(palindromeTester.isWordPresent(" hii world","hello"));
+    assertTrue(palindromeTester.isWordPresent(" hello world","hello"));
 	
 	
 
